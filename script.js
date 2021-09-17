331eebc1755a1b3bcc87d292ce1f09b8639495c6
PushkarevAR <pm.pushkarev.ar@gmail.com>
2021-09-17 21:24:59 +0300

    Added main-style.css
    Styles for main block & colors remake

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ colorSwitcherIcon.addEventListener("click", () => {
 let btns = document.querySelectorAll(".color");
 let root = document.querySelector(":root");
 
-for(let btn of btns){
+for (let btn of btns) {
     btn.addEventListener("click", (e) => {
         let target = e.target;
         let open = document.querySelector(".open");
 
-        if(open) open.classList.remove("open");
+        if (open) open.classList.remove("open");
 
         document.querySelector(".active").classList.remove("active");
         target.classList.add("active");
@@ -21,25 +21,45 @@ for(let btn of btns){
         let dataColor = target.getAttribute("data-colors");
         let color = dataColor.split(" ");
 
-        root.style.setProperty("--white", color[0]);
-        root.style.setProperty("--light", color[1]);
-        root.style.setProperty("--dark", color[2]);
-        root.style.setProperty("--hover", color[3]); 
+        root.style.setProperty("--black", color[0]);
+        root.style.setProperty("--main", color[1]);
+        root.style.setProperty("--main-hove", color[2]);
+        root.style.setProperty("--btn", color[3]);
+        root.style.setProperty("--btn-hover", color[4]);
+        root.style.setProperty("--light-bg", color[5]);
     })
 }
 
-let darkTheme = ["grey", "grey", "grey", "grey"];
+/* --white: #fff;
+--black: #181818;
+--main: #222222;
+--main-hover: #222222cc;
+--btn: #1a1a1a;
+--btn-hover: #1a1a1a80;
+--light-bg: #2e2e2e; */
+
+let darkTheme = ["#181818",
+    "#222222",
+    "#222222cc",
+    "#1a1a1a",
+    "#1a1a1a80",
+    "#2e2e2e"];
 
 let themeSwitcherIcon = document.querySelector(".theme-switcher-icon");
+let intro = document.querySelector(".intro");
 
 themeSwitcherIcon.addEventListener("click", () => {
     themeSwitcherIcon.classList.toggle("dark");
+    intro.classList.toggle("dark");
 
-    if(document.querySelector(".dark")) {
-        root.style.setProperty("--white", darkTheme[0]);
-        root.style.setProperty("--light", darkTheme[1]);
-        root.style.setProperty("--dark", darkTheme[2]);
-        root.style.setProperty("--hover", darkTheme[3]); 
-    } else document.querySelector(".color.blue").click();
+    if (document.querySelector(".dark")) {
+        root.style.setProperty("--black", darkTheme[0]);
+        root.style.setProperty("--main", darkTheme[1]);
+        root.style.setProperty("--main-hove", darkTheme[2]);
+        root.style.setProperty("--btn", darkTheme[3]);
+        root.style.setProperty("--btn-hover", darkTheme[4]);
+        root.style.setProperty("--light-bg", darkTheme[5]);
+    } else {
+        document.querySelector(".color.purple").click();}
 });
 
