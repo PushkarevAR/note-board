64f035cae6016f37f738583f539f37cf19d671f9
PushkarevAR <pm.pushkarev.ar@gmail.com>
2021-09-24 19:31:32 +0300

    redesign
    colors n theme now looks good x0x0

---
+++ src/script/app.js
@@ -0,0 +1,121 @@
+const introArea = document.querySelector(".intro"),
+  introNoteArea = introArea.querySelector(".note-input"),
+  noteTitleInput = introNoteArea.querySelector("input"),
+  noteTextInput = introNoteArea.querySelector("textarea"),
+  btnAddNote = introNoteArea.querySelector("button"),
+  closeNoteInput = introNoteArea.querySelector("i"),
+  inputLengthCounter = introNoteArea.querySelector("span"),
+  notesArea = document.querySelector(".notes-wrapper");
+
+// Focus on Mote input section by intro btn
+document.querySelector(".intro-text").querySelector("button").onclick = () => {
+  noteTitleInput.focus();
+};
+
+// Clear Note input by cross icon
+closeNoteInput.addEventListener("click", clearNoteInput);
+
+function clearNoteInput() {
+  // function clears .note-input
+  noteTextInput.value = "";
+  noteTitleInput.value = "";
+  noteTextInput.placeholder = "Note";
+  noteTitleInput.placeholder = "Title";
+  noteTextInput.style.height = "120px";
+  inputLengthCounter.style.color = "var(--color)";
+  inputLengthCounter.style.visibility = "hidden";
+}
+// Note input validation
+noteTextInput.addEventListener("keyup", (e) => {
+  validated(e.target);
+
+  noteTextInput.style.height = "120px"; // Resize noteTextArea
+  let scHeight = e.target.scrollHeight;
+  if (scHeight > 120) noteTextInput.style.height = `${scHeight}px`;
+});
+
+noteTextInput.addEventListener("keydown", (e) => validated(e.target));
+
+function validated(input) {
+  const inputLimit = 100,
+    inputLength = input.value.length;
+
+  if (inputLength != 0) {
+    inputLengthCounter.style.visibility = "visible";
+    inputLengthCounter.textContent = inputLimit - inputLength;
+
+    if (inputLength <= inputLimit) {
+      btnAddNote.disabled = false;
+      inputLengthCounter.style.color = "var(--color)";
+    } else {
+      btnAddNote.disabled = true;
+      inputLengthCounter.style.color = "var(--error)";
+    }
+  } else {
+    btnAddNote.disabled = false;
+    inputLengthCounter.style.color = "var(--color)";
+    inputLengthCounter.style.visibility = "hidden";
+  }
+}
+
+// Add note by btn "Add note"
+btnAddNote.addEventListener("click", () => {
+  if (isInputEmpty(noteTitleInput) || isInputEmpty(noteTextInput)) return;
+
+  if (!notesArea.querySelector(".note")) {
+    // move intro on top
+    introArea.style.marginTop = "100px";
+    notesArea.style.visibility = "visible";
+  }
+
+  renderNotes();
+  clearNoteInput();
+});
+
+function renderNotes() {
+  let note = document.createElement("div");
+
+  note.classList.add("note");
+  note.innerHTML = `<i class="fas fa-times-circle"></i>
+    <h3>${noteTitleInput.value}</h3>
+    <p>${noteTextInput.value.replace(/\n\r?/g, "<br />")}</p>`;
+
+  notesArea.append(note);
+}
+
+function isInputEmpty(input) {
+  if (input.value == "") {
+    input.style.backgroundColor = "var(--error)";
+    input.style.transform = "rotate(0.5deg) scale(1.1) translateY(2px)";
+    input.placeholder = "Type something here...";
+
+    setTimeout(() => {
+      input.style.backgroundColor = "var(--white)";
+      input.style.transform = "none";
+    }, 100);
+    input.focus();
+    return true;
+  } else return false;
+}
+
+// delete notes and hide notesArea if there r no notes at all
+notesArea.addEventListener("click", function (event) {
+  let target = event.target;
+
+  if (target.tagName != "I") return; // out if click not on close btn
+
+  target.parentNode.style.opacity = 0.5; // just to make it smoothe
+  
+  setTimeout(() => {
+    target.parentNode.remove();
+
+    if (!notesArea.querySelector(".note")) { // if there r no notes at all -> move intro
+      introArea.style.marginTop = "20%";
+      notesArea.style.visibility = "hidden";
+    }
+  }, 80);
+});
+
+Sortable.create(notesArea, { //making notes draggble xD
+  animation: 150,
+});
---
+++ src/script/appearance.js
@@ -0,0 +1,104 @@
+//-----------------------theme and colors changers-----------------------
+const colorSwitcherIcon = document.querySelector(".color-switcher-icon"),
+  colorSwitcherDropdown = document.querySelector(".color-switcher-dropdown");
+
+colorSwitcherIcon.addEventListener("click", () => {
+  colorSwitcherDropdown.classList.toggle("open");
+});
+
+window.addEventListener("click", (e) => {
+  // shitty KOSTILI
+  if (
+    e.target == document.querySelector("main") &&
+    document.querySelector(".open")
+  ) {
+    colorSwitcherDropdown.classList.remove("open");
+  }
+});
+
+const btnColors = document.querySelectorAll(".color"),
+  root = document.querySelector(":root");
+
+const darkTheme = ["#1d1d1d", "#fcfcfc", "#181818", "rgb(43, 43, 43)"];
+
+const lightTheme = ["#fff", "#252525", "#f7f7f7", "rgb(235, 235, 235)"];
+
+const themeSwitcherIcon = document.querySelector(".theme-switcher-icon"),
+  mainWrapper = document.querySelector(".main-wrapper");
+
+for (let btn of btnColors) {
+  btn.addEventListener("click", changeColor);
+}
+
+function changeColor(e) {
+  let target = e.target;
+  let open = document.querySelector(".open");
+
+  if (open) open.classList.remove("open");
+
+  document.querySelector(".active").classList.remove("active");
+  target.classList.add("active");
+
+  let dataColor = target.getAttribute("data-colors");
+  let color = dataColor.split(" ");
+
+  root.style.setProperty("--color", color[0]);
+  root.style.setProperty("--color-hover", color[1]);
+}
+
+themeSwitcherIcon.addEventListener("click", () => {
+  themeSwitcherIcon.classList.toggle("dark");
+
+  if (document.querySelector(".dark")) {
+    root.style.setProperty("--white", darkTheme[0]);
+    root.style.setProperty("--black", darkTheme[1]);
+    root.style.setProperty("--main", darkTheme[2]);
+    root.style.setProperty("--grey", darkTheme[3]);
+  } else {
+    root.style.setProperty("--white", lightTheme[0]);
+    root.style.setProperty("--black", lightTheme[1]);
+    root.style.setProperty("--main", lightTheme[2]);
+    root.style.setProperty("--grey", lightTheme[3]);
+  }
+});
+
+//-----------------------Hide header on scroll-----------------------
+(function () {
+  const doc = document.documentElement,
+    w = window;
+
+  let prevScroll = w.scrollY || doc.scrollTop;
+  let curScroll,
+    direction = 0,
+    prevDirection = 0;
+
+  let header = document.querySelector("header");
+
+  let checkScroll = function () {
+    curScroll = w.scrollY || doc.scrollTop;
+
+    if (curScroll > prevScroll) {
+      direction = 2;
+    } else if (curScroll < prevScroll) {
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+
+    prevScroll = curScroll;
+  };
+
+  let toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 60) {
+      header.classList.add("hide");
+      prevDirection = direction;
+    } else if (direction === 1) {
+      header.classList.remove("hide");
+      prevDirection = direction;
+    }
+  };
+
+  window.addEventListener("scroll", checkScroll);
+})();
